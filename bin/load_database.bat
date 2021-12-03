b600a417593d89524010bc237c63b98d14a31e00
ashut0shk <ashut0shk@outlook.com>
2021-12-03 23:19:17 +0530

    Most Dragons can spit fire, but few learned to Fly high
    Signed-off-by: Ashutosh Kumar <kumar.ashutosh@siemens.com>

---
+++ bin/load_database.bat
@@ -0,0 +1,2 @@
+"D:/XXXXXX/bin/wamp/bin/mariadb/mariadb10.4.10/bin/mysql"  -u developer -p########  < ./schema.sql
+"D:/XXXXXX/bin/wamp/bin/mariadb/mariadb10.4.10/bin/mysql"  -u developer -p########  < ./data.sql
