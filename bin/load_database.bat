6779b8078eea889ec56a02d3dfdecc3c841e1b99
ashut0shk <ashut0shk@xxxxxxx.com>
2021-12-03 23:22:02 +0530

    Most Dragons can spit fire, but few learned to Fly high
    Signed-off-by: Ashutosh Kumar <kumar.ashutosh@siemens.com>

--- bin/load_database.bat
+++ bin/load_database.bat
@@ -1,2 +1,6 @@
+Rem change directory path to the mysql binary
+Rem change the password for DB user
+
 "D:/XXXXXX/bin/wamp/bin/mariadb/mariadb10.4.10/bin/mysql"  -u developer -p########  < ./schema.sql
 "D:/XXXXXX/bin/wamp/bin/mariadb/mariadb10.4.10/bin/mysql"  -u developer -p########  < ./data.sql
+
